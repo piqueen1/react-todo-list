927cbdec5793bff8c27c6a2f2d23297ed3e4ee6d
Finn Thye <piqueen@gmail.com>
2021-03-06 08:45:45 -0700

    Add functional component

--- react-todo-list/src/index.js
+++ react-todo-list/src/index.js
@@ -1,16 +1,22 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
+function MyApp() {
+  return (
+    <div>
+      <h2>What my kids are doing this morning</h2>
+      <ul>
+        <li>Read books to each other</li>
+        <li>Show each other tablet games</li>
+        <li>Eat cuties and bacon and eggs</li>
+        <li>Put on bathingsuits</li>
+        <li>Go get in the hot springs</li>
+      </ul>
+    </div>
+  )
+}
+
 ReactDOM.render(
-  <div>
-    <h2>What my kids are doing this morning</h2>
-    <ul>
-      <li>Read books to each other</li>
-      <li>Show each other tablet games</li>
-      <li>Eat cuties and bacon and eggs</li>
-      <li>Put on bathingsuits</li>
-      <li>Go get in the hot springs</li>
-    </ul>
-  </div>, 
+  <MyApp />,
   document.getElementById("root")
   )
