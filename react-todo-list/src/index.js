52929665046aca68f28b4f7a9b6e663b81751f71
Finn Thye <piqueen@gmail.com>
2021-03-08 09:16:59 -0700

    Created/populated App.js

--- react-todo-list/src/index.js
+++ react-todo-list/src/index.js
@@ -1,10 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import MyInfo from "./components/MyInfo.js"
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App.js"
 
-
-
-ReactDOM.render(
-  <MyInfo />,
-  document.getElementById('root')
-  )
+ReactDOM.render(<App />, document.getElementById('root'))
