bc2fac5b586b6593912297568c894b40faa063bb
Finn Thye <piqueen@gmail.com>
2021-03-08 09:08:46 -0700

    Made new MyInfo.js and MyInfo.css; migrated code from index

--- react-todo-list/src/index.js
+++ react-todo-list/src/index.js
@@ -1,21 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import MyInfo from "./components/MyInfo.js"
 
-function MyInfo() {
-  return (
-    <div className="main">
-      <h1>Finn Thye</h1>
-      <p>I'm a poet, a mom, a recovering addict, a web developer and a linguist.</p>
-      <p>Powerhouse femeninity, battleaxe fairy godmother, I've got your back.</p>
-      <ol>
-        <li>Princeton Hot Springs</li>
-        <li>Prague</li>
-        <li>Blackberry-brambled cemeteries in Wales</li>
-      </ol>
-    </div>
-  )
-}
+
 
 ReactDOM.render(
   <MyInfo />,
